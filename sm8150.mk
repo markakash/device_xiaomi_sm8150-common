df9d6c86d88bf30fb14beaab4777d90c24fa3825
Akash Srivastava <akashniki@gmail.com>
2019-11-06 23:41:39 +0100

    sm8150-common: Remove livedisplay

--- sm8150.mk
+++ sm8150.mk
@@ -200,10 +200,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm8150
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
